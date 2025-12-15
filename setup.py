c58509451fb0b7e8b2939b54db2acb96a2a29387
Mohamed Atef <mo5460642@gmail.com>
2025-12-15 14:12:47 +0200

    Requirements Added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",        
+    author="mohamed Atef",
+    author_email="mohamedatefsallam7@gmail.com",
+    packages=find_packages(),
+    install_requires=[],  # Dependencies are now managed in requirements.txt
+
+)
