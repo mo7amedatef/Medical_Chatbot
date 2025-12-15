702e385d5b4f2abc32c7e43b5170101ddd95d4f4
Mohamed Atef <mo5460642@gmail.com>
2025-12-15 12:55:17 +0200

    Project Structure added

---
+++ template.sh
@@ -0,0 +1,17 @@
+# create project directories
+mkdir -p src
+mkdir -p research
+
+# Create files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+touch README.md
+
+
+echo "Project structure created successfully."
